abf5ba2c8614d872a8d6c6b9e193569dabc9aac4
Chris Kinney <christopher.kinney.2@gmail.com>
2023-05-17 06:23:33 -0500

    Card Props Added and Aligned for Mobile Devices

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,11 +1,14 @@
 import { Slot, component$, useStylesScoped$ } from '@builder.io/qwik';
 import CardStyles from './card.css?inline';
 
+interface CardProps {
+    size: 'sm' | 'lg';
+}
 
-export default component$(() => {
+export default component$((props: CardProps) => {
     useStylesScoped$(CardStyles);
     return (
-        <div class='card-wrapper'>
+        <div class={`card-wrapper ${props.size}`}>
             <div class='card-content'>
                 <Slot />
             </div>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,7 +7,7 @@ export default component$(() => {
   return (
     <div class='bContainer'>
       <div id='card1'>
-        <Card>
+        <Card size='sm'>
           <p>Hello!  My name is Christopher Kinney, but you may call me Chris!
           I am a full stack developer with a passion for learning and a love for problem solving.
           I love to work with new technologies and developing new projects. I am interested in starting new projects, so feel free to contact me with your offer!</p>
@@ -16,7 +16,7 @@ export default component$(() => {
       <div class='aboutMe'>About Me</div>
       <div class='container'>
         <div>
-          <Card>
+          <Card size='sm'>
             <p>I am a veteran of the United States Air Force.  I served for six years in aviation command and control. My love for technology started with learning 
               about radios and communication networking with different frequencies. During that time I have gotten to travel to many different locations
               to include: South Korea, Japan, UAE, and Ireland.
@@ -24,7 +24,7 @@ export default component$(() => {
           </Card>
         </div>
         <div>
-          <Card>
+          <Card size='sm'>
             <p>I am  a self-taught developer with about 1 year of experience.
               My passion for learning has lead me to develop a few different skill sets.  I am a big fan of aviation and fly whenever I can afford to do so.
               If I am not flying, I am usually looking up the newest technology and trying to find fun and new ways to implement it.
@@ -33,7 +33,7 @@ export default component$(() => {
           </Card>
         </div>
         <div >
-          <Card>
+          <Card size='sm'>
             <p>My list of hobbies seem to grow by the day, but I would say that I am an avid gamer. I also enjoy spending time with my two dogs and disc golfing
               whenever I can. Camping, mountain hiking, and fishing are some of my favorite outdoor pasttimes. Next year I plan on moving to Alaska where I can enjoy
               more of the outdoors in my free time.</p>
@@ -42,13 +42,13 @@ export default component$(() => {
       </div>
       <div class='aboutMe'>Skills</div>
       <div class='card'>
-        <Card>
+        <Card size='lg'>
           <p>List of Skills</p>
         </Card>
       </div>
       <div class='aboutMe'>Projects</div>
       <div class='card'>
-        <Card>
+        <Card size='lg'>
           <p> Projects</p>
         </Card>
       </div>
